0070cb5d006ae56883cfc453e0b9fbdc9ada4226
madact1 <57218352+madact1@users.noreply.github.com>
2020-05-02 16:30:54 +0100

    Added comments

--- Dashboard/scripts.js
+++ Dashboard/scripts.js
@@ -206,8 +206,8 @@ function getUnderControlDoughnutGraph(){ //function used to create the chart and
 getUnderControlDoughnutGraph(); //initiate the chart function to begin webpage development
 
 
-//Gauge
-
+//Gauge by Charlotte
+//Setting the scaling for each section
 var ScalingFactor = function(a) {
   return Math.round(a);
 };
@@ -218,15 +218,15 @@ var randomData = function () {
     ScalingFactor(2),
     ScalingFactor(3),
   ];
-};
+}; //Use to mess around with random valuves but now they have been split into 3 sections. More could be added if need be.
 
 var randomValue = function (data) {
   return Math.max.apply(null, data) * Math.random();
-};
+}; //Setting a random value to the needle to generate everytime the page loads.
 
 var data = randomData();
 var value = randomValue(data);
-
+//Setting up the data. This is where an API can be added.
 var config = {
   type: 'gauge',
   data: {
@@ -240,30 +240,31 @@ var config = {
   },
   options: {
     responsive: true,
+		//title settings
     title: {
       display: true,
       text: 'Risk of Bush fire today based on humidity and current tempurature',
-    },
+    },//layout settings
     layout: {
       padding: {
         bottom: 30
       }
     },
     needle: {
-      // Needle circle radius as the percentage of the chart area width
+      // Needle circle radius
       radiusPercentage: 2,
-      // Needle width as the percentage of the chart area width
+      // Needle width
       widthPercentage: 3.2,
-      // Needle length as the percentage of the interval between inner radius (0%) and outer radius (100%) of the arc
+      // Needle length
       lengthPercentage: 60,
       // The color of the needle
       color: '#353b48'
     },
-    valueLabel: {
+    valueLabel: {//Disables numbers from being displayed
       display: false
     },
     plugins: {
-      datalabels: {
+      datalabels: {//Displays the data labels , including editting settings such as the size, font family, etc
         display: true,
         formatter:  function (value, context) {
           return context.chart.data.labels[context.dataIndex];
@@ -283,12 +284,12 @@ var config = {
 window.onload = function() {
   var ctx = document.getElementById('chart').getContext('2d');
   window.myGauge = new Chart(ctx, config);
-};
+};//This is loaded when the HTML is so it doesn't go before it. This also links the HTML element to the javscript
 
 document.getElementById('randomizeData').addEventListener('click', function() {
   config.data.datasets.forEach(function(dataset) {
     dataset.data = randomData();
-    dataset.value = randomValue(dataset.data);
+    dataset.value = randomValue(dataset.data); //Sets up the random data for when the page loads
   });
 
   window.myGauge.update();
